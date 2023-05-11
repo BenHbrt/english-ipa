b3930232b4d76692b68f4b22190c577911006e71
BenHbrt <benherbert1983@googlemail.com>
2023-05-11 16:53:14 +0200

    Testing audio player

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './Global.scss'
 import './App.scss';
 
 function App() {
+
+  const playAudio = (name) => {
+    new Audio(require(`./Audio/Phonemes/PhAI.m4a`)).play();
+  }
+
   return (
     <div className="App">
       <h1>App</h1>
+      <button onClick={playAudio}>Audio</button>
     </div>
   );
 }
